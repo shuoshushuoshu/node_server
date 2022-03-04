1948d7f0e73781a7c48551bcc92f050a174012a5
unknown <luff@mico.com>
2022-03-04 17:50:09 +0800

    push

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({
+    name:'liushuo',
+    age: 25,
+    sex:'male'
+  })
 });
 
 module.exports = router;
