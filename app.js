72e223c156d04b9df86198ee77d3382ed022413f
unknown <luff@mico.com>
2022-03-11 11:36:50 +0800

    push

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req,res,next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8081/#/');
+  res.header('Access-Control-Allow-Origin', null);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   // res.writeHead(200, {
   //   'Content-Type': 'application/json;charset=utf-8',
