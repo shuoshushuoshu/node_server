ba9cac242aabd3157c4176be123865cf2bac7d9d
unknown <luff@mico.com>
2022-03-11 11:11:48 +0800

    push

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
+app.options('*', cors())
+// app.options("/*", function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//   res.sendStatus(200);
+// });
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +31,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req,res,next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://192.168.X.X:4200');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.writeHead(200, {
+    'Content-Type': 'application/json;charset=utf-8',
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Origin':'*'
+  })
   next();
 })
 app.use('/', indexRouter);
